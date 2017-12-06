a3a9e799a5f700f6132b7e8f27f0a95a90aaed8f
joshua murray <joshuajmurray@gmail.com>
2017-12-05 17:45:03 -0800

    model added:

--- app/src/main/java/com/jmurray/android/namemanglerrevenge/NameMangler.java
+++ app/src/main/java/com/jmurray/android/namemanglerrevenge/NameMangler.java
@@ -1,8 +1,35 @@
 package com.jmurray.android.namemanglerrevenge;
 
+import java.util.Random;
+
 /**
  * Created by joshua on 12/5/2017.
  */
 
 public class NameMangler {
+    private static String[] mNiceNames;
+    private static String[] mRudeNames;
+    private String mMangledName;
+    private String mFirstName;
+    private boolean mIsNice;
+
+    public NameMangler(String name, boolean isNice) {
+        mFirstName = name;
+        mIsNice = isNice;
+        mNiceNames = new String[]{"Magnificent", "Awesome", "Special", "Splendid","Majestic"};
+        mRudeNames = new String[]{"Arseface", "Moron", "Fool", "Imbecile","Nincompoop"};
+    }
+
+    public String getMangledName(){
+
+        Random rand = new Random();
+
+        if(mIsNice) {
+            mMangledName = mNiceNames[rand.nextInt(mNiceNames.length)];
+        } else {
+            mMangledName = mRudeNames[rand.nextInt(mRudeNames.length)];
+        }
+        return mMangledName;
+    }
+
 }
