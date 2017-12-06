13276bdfe5654e361a6abc4ca379ddd588952c7f
joshua murray <joshuajmurray@gmail.com>
2017-12-05 17:54:58 -0800

    add nice activity and view

---
+++ app/src/main/java/com/jmurray/android/namemanglerrevenge/NiceActivity.java
@@ -0,0 +1,13 @@
+package com.jmurray.android.namemanglerrevenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nice);
+    }
+}
